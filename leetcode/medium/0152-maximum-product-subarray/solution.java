7ecf52e5f415fb4c4bf018bc5fd9c689f8f61701
Abhijeetjadhav1234 <abhijeetjadhavas@gmail.com>
2026-07-12 17:57:24 +0530

    ✅ Maximum Product Subarray | Medium | Java | Runtime: 2 ms (70.89%) | Memory: 46.9 MB | 2026-07-12

--- leetcode/medium/0152-maximum-product-subarray/solution.java
+++ leetcode/medium/0152-maximum-product-subarray/solution.java
@@ -1,13 +1,17 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int ans=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            int pro=1;
-            for(int j=i;j<nums.length;j++){
-                pro*=nums[j];
-                ans=Math.max(ans,pro);
-            }
-        }
-        return ans;
+      int prefix=1;
+      int sufix=1;
+      int ans=nums[0];
+      int n=nums.length;
+      for(int i=0;i<nums.length;i++){
+        if(prefix==0)prefix=1;
+        if(sufix==0)sufix=1;
+        prefix*=nums[i];
+        sufix*=nums[n-i-1];
+        int current=Math.max(prefix,sufix);
+        ans=Math.max(ans,current);
+      }  
+      return ans;
     }
 }
