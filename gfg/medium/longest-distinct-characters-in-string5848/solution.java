44d44f6c5fac7eca1661fd0e2407b2b869a030b4
Abhijeetjadhav1234 <abhijeetjadhavas@gmail.com>
2026-08-09 13:19:24 +0530

    ✅ Longest Substring with Distinct Characters | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-09

--- gfg/medium/longest-distinct-characters-in-string5848/solution.java
+++ gfg/medium/longest-distinct-characters-in-string5848/solution.java
@@ -2,19 +2,17 @@ class Solution {
     public int longestUniqueSubstr(String s) {
         // code here
         int maxcnt=0;
-        for(int i=0;i<s.length();i++){
-            Set<Character>set=new TreeSet<>();
-            int cnt=0;
-            for(int j=i;j<s.length();j++){
-                char ch=s.charAt(j);
-                if(!set.contains(ch)){
-                    set.add(ch);
-                    maxcnt=Math.max(maxcnt,set.size());
-                }
-                else{
-                    break;
-                }
+        int left=0;
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int rigth=0;rigth<s.length();rigth++){
+            char ch=s.charAt(rigth);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            while(map.get(ch)>1){
+                char cl=s.charAt(left);
+                map.put(cl,map.get(cl)-1);
+                left++;
             }
+            maxcnt=Math.max(maxcnt,rigth-left+1);
         }
         return maxcnt;
     }
