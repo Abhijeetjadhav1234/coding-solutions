be1cd7589b3c9d2cea6ea640f872cd333ee690b8
Abhijeetjadhav1234 <abhijeetjadhavas@gmail.com>
2026-08-18 14:20:28 +0530

    ✅ Daily Temperatures | Medium | Java | Runtime: 78 ms (18.06%) | Memory: 106.6 MB | 2026-08-18

--- leetcode/medium/0739-daily-temperatures/solution.java
+++ leetcode/medium/0739-daily-temperatures/solution.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n=temperatures.length;
-        int [] ans=new int[n];
-        Arrays.fill(ans,0);
-        Stack<Integer>st=new Stack<>();
+        int[] ans=new int[n];
+        Stack<Integer>s=new Stack<>();
         for(int i=n-1;i>=0;i--){
-           while(!st.isEmpty() && temperatures[st.peek()]<=temperatures[i]){
-            st.pop();
-           }
-           if(!st.isEmpty()){
-            ans[i]=st.peek()-i;
-           }
-           st.push(i);
-        
+          while(!s.isEmpty() && temperatures[s.peek()]<=temperatures[i]){
+            s.pop();
+          }
+         if(!s.isEmpty()){
+          ans[i]=s.peek()-i;
+         }
+         s.push(i);
+ 
         }
         return ans;
     }
