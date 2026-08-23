02239cfeb602ce3bf158ec3d0fd91e8ab28829cb
Abhijeetjadhav1234 <abhijeetjadhavas@gmail.com>
2026-08-23 18:31:25 +0530

    ✅ Evaluate Reverse Polish Notation | Medium | Java | Runtime: 579 ms (5.50%) | Memory: 47 MB | 2026-08-23

--- leetcode/medium/0150-evaluate-reverse-polish-notation/solution.java
+++ leetcode/medium/0150-evaluate-reverse-polish-notation/solution.java
@@ -1,27 +1,26 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer>stack=new Stack<>();
-        for(String t:tokens){
-            if(t.equals("+") || t.equals("-") || t.equals("*") || t.equals("/")){
-                int a=stack.pop();
-                int b=stack.pop();
-                int val=0;
-                if (t.equals("+"))
-                    val = b + a;
-                else if (t.equals("-"))
-                    val = b - a;
-                else if (t.equals("*"))
-                    val = b * a;
-                else
-                    val = b / a;
-
-                stack.push(val);
-                            }
-                            else{
-                                stack.push(Integer.parseInt(t));
-                            }
+        List<String>l=new ArrayList<>(Arrays.asList(tokens));
+        while(l.size()>1){
+            for(int i=0;i<l.size();i++){
+                String t=l.get(i);
+                if(t.equals("+") || t.equals("-") || t.equals("*")|| t.equals("/")){
+                    int b=Integer.parseInt(l.get(i-1));
+                    int a=Integer.parseInt(l.get(i-2));
+                    int val=0;
+                    if(t.equals("+"))val=a+b;
+                    else if(t.equals("-"))val=a-b;
+                    else if(t.equals("*"))val=a*b;
+                    else val=a/b;
+                    
+                    l.set(i-2,String.valueOf(val));
+                    l.remove(i);
+                    l.remove(i-1);
+                    break;
+                }
+            }
         }
 
-        return stack.peek();
+        return Integer.parseInt(l.get(0));
     }
 }
