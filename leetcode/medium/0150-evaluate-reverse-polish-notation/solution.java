548c9ed06429e534ab457deae9bfd3bd135ce55a
Abhijeetjadhav1234 <abhijeetjadhavas@gmail.com>
2026-08-23 19:17:31 +0530

    ✅ Evaluate Reverse Polish Notation | Medium | Java | Runtime: 7 ms (35.17%) | Memory: 45.6 MB | 2026-08-23

--- leetcode/medium/0150-evaluate-reverse-polish-notation/solution.java
+++ leetcode/medium/0150-evaluate-reverse-polish-notation/solution.java
@@ -1,26 +1,20 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        List<String>l=new ArrayList<>(Arrays.asList(tokens));
-        while(l.size()>1){
-            for(int i=0;i<l.size();i++){
-                String t=l.get(i);
-                if(t.equals("+") || t.equals("-") || t.equals("*")|| t.equals("/")){
-                    int b=Integer.parseInt(l.get(i-1));
-                    int a=Integer.parseInt(l.get(i-2));
-                    int val=0;
-                    if(t.equals("+"))val=a+b;
-                    else if(t.equals("-"))val=a-b;
-                    else if(t.equals("*"))val=a*b;
-                    else val=a/b;
-                    
-                    l.set(i-2,String.valueOf(val));
-                    l.remove(i);
-                    l.remove(i-1);
-                    break;
-                }
+       Stack<Integer>s=new Stack<>();
+        for(String t:tokens){
+            if(t.equals("+") || t.equals("/") ||t.equals("*")||t.equals("-")){
+            int b=s.pop();
+            int a=s.pop();
+            int val=0;
+            if(t.equals("+"))val=a+b;
+            else if(t.equals("-"))val=a-b;
+            else if(t.equals("*"))val=a*b;
+            else val=a/b;
+            s.push(val);}
+            else{
+                s.push(Integer.parseInt(t));
             }
         }
-
-        return Integer.parseInt(l.get(0));
+        return s.peek();
     }
 }
